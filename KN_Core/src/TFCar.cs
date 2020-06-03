7017d9b04a9149df550c51ecad36935f2b0986ef
trbflxr <igor.tsarev17@gmail.com>
2020-06-04 00:06:58 +0300

    fixed car == operator

--- KN_Core/src/TFCar.cs
+++ KN_Core/src/TFCar.cs
@@ -50,8 +50,7 @@ namespace KN_Core {
 
       return car0.Name == car1.Name &&
              car0.Id == car1.Id &&
-             car0.IsGhost == car1.IsGhost &&
-             car0.IsNetworkCar == car1.IsNetworkCar;
+             car0.IsGhost == car1.IsGhost;
     }
 
     public static bool operator !=(TFCar car0, TFCar car1) {
