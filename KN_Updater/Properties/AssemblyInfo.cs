ab0272ba08c9f87cac2905ca47e94a997ce88b60
trbflxr <igor.tsarev17@gmail.com>
2020-09-30 00:59:54 +0300

    updated assembly info for KN_Updater

--- KN_Updater/Properties/AssemblyInfo.cs
+++ KN_Updater/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KN_Updater")]
-[assembly: AssemblyDescription("Updater for KiNO mod")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Updater for KiNO mod")]
+[assembly: AssemblyDescription("Needed to automatically update KiNO mod")]
+[assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KN_Updater")]
 [assembly: AssemblyCopyright("Copyright © trbflxr 2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("KiNO")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
