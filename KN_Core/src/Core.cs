3e55e6ee32131604d54873240530382dfee2e4fe
trbflxr <igor.tsarev17@gmail.com>
2020-09-30 19:21:59 +0300

    updater location check

--- KN_Core/src/Core.cs
+++ KN_Core/src/Core.cs
@@ -82,6 +82,8 @@ namespace KN_Core {
     private static Assembly assembly_;
 
     public Core() {
+      CheckUpdaterLocation();
+
       Changelog.Initialize();
       latestVersion_ = Changelog.GetVersion();
       changelog_ = Changelog.GetChangelog();
@@ -556,6 +558,15 @@ namespace KN_Core {
       }
       else {
         Log.Write("[KN_Core]: Unable to start updater");
+      }
+    }
+
+    private void CheckUpdaterLocation() {
+      string updater = Paths.PluginPath + Path.DirectorySeparatorChar + "KN_Updater.exe";
+
+      if (!Directory.Exists(updater)) {
+        Log.Write($"[KN_Core]: Unable to locate updater at '{updater}'");
+        throw new Exception($"[KN_Core]: Unable to locate updater at '{updater}'");
       }
     }
 
