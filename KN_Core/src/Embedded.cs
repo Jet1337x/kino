86141f7c4a36a9a49236e1ee5cfc71e30fe7de34
trbflxr <igor.tsarev17@gmail.com>
2020-10-10 16:06:32 +0300

    fixed texture loading

--- KN_Core/src/Embedded.cs
+++ KN_Core/src/Embedded.cs
@@ -35,12 +35,13 @@ namespace KN_Core {
             stream.CopyTo(memoryStream);
             tex.LoadImage(memoryStream.ToArray());
 
-            for (int i = 0; i < size; ++i) {
-              for (int j = 0; j < size; ++j) {
+            for (int i = 0; i < tex.width; ++i) {
+              for (int j = 0; j < tex.height; ++j) {
                 tex.SetPixel(i, j, tex.GetPixel(i, j) * mask);
               }
             }
-            tex.Apply(false);
+
+            tex.Apply(true);
           }
           else {
             for (int i = 0; i < size; ++i) {
